87a14b007a93ae22b5b973757b0d59be2f2c0835
Artyom Zalin <z.lasekofeun@gmail.com>
2026-04-18 03:02:50 +0300

    лабораторная 9 джанго

---
+++ news/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import News, Category
+def index(request):
+    news = News.objects.all()
+    context={
+        'news': news,
+        'title': 'Список новостей',
+    }
+    return render(request, template_name='news/index.html', context=context)
+def get_category(request, category_id):
+    news=News.objects.filter(category_id=category_id)
+    category=Category.objects.get(pk=category_id)
+    context={
+        'news': news,
+        'category': category
+    }
+    return render(request, 'news/category.html', context)
+def test(request):
+    return HttpResponse('<h1>Тестовая страница</h1>')
